aabc2d2cefeeac823cd9e09aba6cf1e04b97fa1c
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-05-09 14:53:51 +0300

    feat: добавить функциональность формы

--- kubansite/pages/forms.py
+++ kubansite/pages/forms.py
@@ -7,29 +7,34 @@ class ContactForm(forms.Form):
                  ('2', 'Юр.лицо'),
                  ('3', 'ИП')))  # Выбор категории клиента
     first_name = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Ваше имя'}))  # Имя
+        widget=forms.TextInput(attrs={'placeholder': 'Ваше имя', 'class': 'form__input'}))  # Имя
     legal_entity = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'Юридическое лицо'}))  # Юридическое лицо
-    inn = forms.IntegerField(
+            attrs={'placeholder': 'Юридическое лицо', 'class': 'form__input'}))  # Юридическое лицо
+    inn = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'ИНН'}), min_value=12, max_value=12)  # ИНН
+            attrs={'placeholder': 'ИНН', 'class': 'form__input'}), max_length=12)  # ИНН
     phone_number = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'placeholder': '+7-999-999-99-99'}))  # Номер телефона
+        widget=forms.NumberInput(
+            attrs={'placeholder': '+7-999-999-99-99', 'class': 'form__input', 'type': 'tel', 'pattern': '\+?[0-9\s\-\(\)]+'}))  # Номер телефона
     e_mail = forms.EmailField(widget=forms.EmailInput(
-        attrs={'placeholder': 'E-mail'}), max_length=50)  # Электронная почта
+        attrs={'placeholder': 'E-mail', 'class': 'form__input'}), max_length=50)  # Электронная почта
     region = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'Ваш регион'}), max_length=100)  # Регион
+            attrs={'placeholder': 'Ваш регион', 'class': 'form__input'}), max_length=100)  # Регион
     sales_channel = forms.ChoiceField(
-        choices=(('1', 'Retail'), ('2', 'Дистрибуция'), ('3', 'Физ.лицо')))
+        choices=(('1', 'Retail'), ('2', 'Дистрибуция'), ('3', 'Физ.лицо')),
+        widget=forms.Select(attrs={'class':'form__select'}))
     nalog = forms.ChoiceField(
-        choices=(('1', 'НДС'), ('2', 'Без НДС')))   # Вид налога
+        choices=(('1', 'НДС'), ('2', 'Без НДС')),
+        widget=forms.Select(attrs={'class':'form__select'}))   # Вид налога
     theme = forms.ChoiceField(
         choices=(('1', 'Запрос на сотрудничество'),
-                 ('2', 'Вопрос по качеству'), ('3', 'Другое')))
+                 ('2', 'Вопрос по качеству'), ('3', 'Другое')),
+                 widget=forms.Select(attrs={'class':'form__select'}))
     text = forms.CharField(
         widget=forms.Textarea(
-            attrs={'placeholder': 'Сообщение'}), max_length=100)
-    rules = forms.BooleanField(label ='Я принимаю условия пользовательского солгашения')
+            attrs={'placeholder': 'Сообщение', 'class': 'form__input'}), max_length=100)
+    rules = forms.BooleanField(label ='Я принимаю условия пользовательского солгашения',
+        widget=forms.CheckboxInput(
+            attrs={'class': 'form__input contact__checkbox'}))
