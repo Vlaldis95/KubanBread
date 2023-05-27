cdcf540bacabe39458770e0fea592ba61771c3d3
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-05-27 21:18:28 +0300

    fix: исправить форму

--- kubansite/pages/forms.py
+++ kubansite/pages/forms.py
@@ -4,29 +4,21 @@ from django import forms
 class ContactForm(forms.Form):
     first_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'Ваше имя', 'class': 'popup__input'}))  # Имя
-    legal_entity = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'placeholder': 'Юридическое лицо',
-                   'class': 'popup__input'}))  # Юридическое лицо
-    inn = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'placeholder': 'ИНН',
-                   'class': 'popup__input'}), max_length=12)  # ИНН
+            attrs={'placeholder': '*Ваше имя', 'class': 'popup__input'}))  # Имя
     phone_number = forms.RegexField(
         regex='^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$',
-        widget=forms.TextInput(attrs={'placeholder': '+7-999-999-99-99',
+        widget=forms.TextInput(attrs={'placeholder': '*+7-999-999-99-99',
                                       'class': 'popup__input',
                                       'type': 'tel',
                                       'pattern': '^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$'}))  # Номер телефона
     e_mail = forms.EmailField(widget=forms.EmailInput(
-        attrs={'placeholder': 'E-mail',
+        attrs={'placeholder': '*Ваш e-mail',
                'class': 'popup__input',
                'pattern': "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+"}),
                 max_length=50)  # Электронная почта
     region = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'Ваш регион',
+            attrs={'placeholder': 'Ваш регион (страна, город, район, населённый пункт)',
                    'class': 'popup__input'}), max_length=100)  # Регион
     sales_channel = forms.ChoiceField(
         choices=(('Retail', 'Retail'),
