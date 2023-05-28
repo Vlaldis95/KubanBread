820468e3fee25f328725cc62c157331238d54bfb
Владислав Лукьяненко <vlaldis@yandex.ru>
2023-05-28 09:21:38 +0300

    форма исрпавлена

--- kubansite/pages/forms.py
+++ kubansite/pages/forms.py
@@ -4,29 +4,23 @@ from django import forms
 class ContactForm(forms.Form):
     first_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'Ваше имя', 'class': 'popup__input'}))  # Имя
-    legal_entity = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'placeholder': 'Юридическое лицо',
-                   'class': 'popup__input'}))  # Юридическое лицо
-    inn = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'placeholder': 'ИНН',
-                   'class': 'popup__input'}), max_length=12)  # ИНН
+            attrs={'placeholder':
+                   '*Ваше имя',
+                     'class': 'popup__input'}))  # Имя
     phone_number = forms.RegexField(
         regex='^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$',
-        widget=forms.TextInput(attrs={'placeholder': '+7-999-999-99-99',
+        widget=forms.TextInput(attrs={'placeholder': '* +7-999-999-99-99',
                                       'class': 'popup__input',
                                       'type': 'tel',
                                       'pattern': '^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$'}))  # Номер телефона
     e_mail = forms.EmailField(widget=forms.EmailInput(
-        attrs={'placeholder': 'E-mail',
+        attrs={'placeholder': '*Ваш e-mail',
                'class': 'popup__input',
                'pattern': "[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+"}),
                 max_length=50)  # Электронная почта
     region = forms.CharField(
         widget=forms.TextInput(
-            attrs={'placeholder': 'Ваш регион',
+            attrs={'placeholder': 'Ваш регион (страна, город, район, населенный пункт)',
                    'class': 'popup__input'}), max_length=100)  # Регион
     sales_channel = forms.ChoiceField(
         choices=(('Retail', 'Retail'),
@@ -36,16 +30,12 @@ class ContactForm(forms.Form):
     nalog = forms.ChoiceField(
         choices=(('-', '-'), ('НДС', 'НДС'), ('Без НДС', 'Без НДС')),
         widget=forms.Select(attrs={'class': 'popup__select'}))   # Вид налога
-    theme = forms.ChoiceField(
-        choices=(('Запрос на сотрудничество', 'Запрос на сотрудничество'),
-                 ('Вопрос по качеству', 'Вопрос по качеству'),
-                 ('Другое', 'Другое')),
-        widget=forms.Select(attrs={'class': 'popup__select'}))
     text = forms.CharField(
         widget=forms.Textarea(
             attrs={'placeholder': 'Сообщение',
-                   'class': 'popup__input'}), max_length=100)
+                   'class': 'popup__input'}), max_length=200)
     rules = forms.BooleanField(
         label='Я принимаю условия пользовательского соглашения',
         widget=forms.CheckboxInput(
-            attrs={'class': 'popup__input service-form__input option-template-input choose-services__option'}))
+            attrs={'class': 
+                   'popup__input service-form__input option-template-input choose-services__option'}))
--- kubansite/pages/views.py
+++ kubansite/pages/views.py
@@ -16,25 +16,19 @@ def contacts(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             body = {'first_name': form.cleaned_data['first_name'],
-                    'legal_entity': form.cleaned_data['legal_entity'],
-                    'inn': form.cleaned_data['inn'],
                     'phone_number': form.cleaned_data['phone_number'],
                     'e_mail': form.cleaned_data['e_mail'],
                     'region': form.cleaned_data['region'],
                     'sales_channel': form.cleaned_data['sales_channel'],
                     'nalog': form.cleaned_data['nalog'],
-                    'theme': form.cleaned_data['theme'],
                     'text': form.cleaned_data['text']}
             ln = '\n'
             message = (f"Имя:{body['first_name']}{ln}"
-                       f"Юр.лицо: {body['legal_entity']}{ln}"
-                       f"ИНН:{body['inn']}{ln}"
                        f"Номер телефона:{body['phone_number']}{ln}"
                        f"e-mail:{body['e_mail']}{ln}"
                        f"Регион:{body['region']}{ln}"
                        f"Канал продаж:{body['sales_channel']}{ln}"
                        f"Вид налога:{body['nalog']}{ln}"
-                       f"Тема сообщения:{body['theme']}{ln}"
                        f"Текст сообщения:{body['text']}")
             try:
                 send_mail("Message from site",
